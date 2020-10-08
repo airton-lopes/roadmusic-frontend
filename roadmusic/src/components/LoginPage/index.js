df439bd90bb4b8469d2d19f9db097c993af8a2a1
airtonlopsan <lopsan@msn.com>
2020-10-08 15:38:57 -0300

    criando pagina de login

---
+++ roadmusic/src/components/LoginPage/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+import { Button, TextField, Typography } from '@material-ui/core';
+
+import { Link, useHistory } from 'react-router-dom';
+
+import useForm from '../../hooks/useForm';
+
+import axios from 'axios';
+
+import { baseUrl } from '../../constants';
+
+
+
+function LoginPage() {
+    const history = useHistory();
+    const { form, onChange } = useForm({
+    emailOrNickname: "",
+    password: ""
+    });
+
+    // const handleLogIn = (e) => {
+    // e.preventDefault()
+    // const body = {
+    //     emailOrNickname: form.emailOrNickname,
+    //     password: form.password
+    // }
+
+    // axios.post(`${baseUrl}/user/login`, body)
+    // .then(response => {
+    //     window.localStorage.setItem("token", response.data.token)
+    //     history.push("/signup")
+    // }).catch(err => {
+    //     console.log('ERRO')
+    // })
+    // }
+    
+    const handleLogIn = async (e) => {
+        e.preventDefault()
+        try {
+        const body = {
+        emailOrNickname: form.emailOrNickname,
+        password: form.password
+    }
+        const response = await axios.post(`${baseUrl}/user/login`, body)
+        window.localStorage.setItem("token", response.data.token)
+        history.push("/signup")
+        } catch (err) {
+        console.log(err)
+        }
+    }
+    
+    const handleInputChange = event => {
+    const { name, value } = event.target;
+
+    onChange(name, value);
+    };
+    return (
+        <div>
+            <Typography variant={'h2'} gutterBottom>RoadMusic</Typography>
+        <form onSubmit={handleLogIn}>
+            <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="emailOrNickname"
+            label="E-mail or Nickname"
+            name="emailOrNickname"
+            autoFocus
+            placeholder="Digite seu E-mail"
+            type="text"
+            value={form.emailOrNickname}
+            onChange={handleInputChange}
+            />
+            <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            placeholder="Digite sua senha"
+            value={form.password}
+            onChange={handleInputChange}
+            />
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            >
+            Entrar
+            </Button>
+        </form>
+            <Link to={'/signup'}>
+            <Button
+                fullWidth
+                variant="outlined"
+                color="primary"
+            >
+                Cadastrar
+            </Button>
+            </Link>
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ roadmusic/src/components/SignUpPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignUpPage() {
+    return (
+        <div>Pagina de SignUpPage</div>
+    );
+}
+
+export default SignUpPage;
