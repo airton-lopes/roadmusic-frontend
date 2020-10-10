909e821e38f7ed94537c73694c8934c19bcaf7c9
airtonlopsan <lopsan@msn.com>
2020-10-10 14:22:58 -0300

    corrigindo signup

--- roadmusic/src/components/SignUpPage/index.js
+++ roadmusic/src/components/SignUpPage/index.js
@@ -1,105 +1,108 @@
-import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import axios from 'axios'
+import { useHistory } from 'react-router-dom';
 import useForm from '../../hooks/useForm';
-import axios from 'axios';
-import { baseUrl } from '../../constants';
-import { Button, TextField, Typography } from '@material-ui/core';
+import { baseUrl } from '../../constants/index';
 
-function SignUpPage() {
-    // const history = useHistory();
+const SignUpPage = () =>{
+    const history = useHistory();
     const { form, onChange } = useForm({
-        name: "",
-        email: "",
-        nickname: "",
-        password: ""
+    name: "",
+    email: "",
+    nickname: "",
+    password: ""
     });
 
-    const handleSignUp = async (e) => {
+    const handleSignUp = (e) => {
         e.preventDefault()
-        try {
         const body = {
             name: form.name,
             email: form.email,
             nickname: form.nickname,
             password: form.password
     }
-        const response = await axios.post(`${baseUrl}/user/signup`, body)
+
+    axios.post(`${baseUrl}/user/signup`, body)
+        .then(response => {
         window.localStorage.setItem("accessToken", response.data.accessToken)
-        // history.push("/signup")
-        console.log('cadastrou!')
-        } catch (err) {
-        console.log(err)
-        }
+        history.push("/home")
+        }).catch(err => {
+        console.log(err.message)
+        })
     }
 
     const handleInputChange = event => {
-    const { name, value } = event.target;
+        const { name, value } = event.target;
 
-    onChange(name, value);
+        onChange(name, value);
     };
-    return (
+
+    return(
         <div>
             <Typography variant={'h2'} gutterBottom>Informe os dados abaixo</Typography>
             <form onSubmit={handleSignUp}>
             <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                id="name"
-                label="Digite um nome de usuario"
-                name="name"
-                type="text"
-                value={form.name}
-                onChange={handleInputChange}
-                fullWidth
+            variant="outlined"
+            margin="normal"
+            required
+            id="user"
+            label="Informe seu nome completo"
+            name="name"
+            type="text"
+            value={form.name}
+            onChange={handleInputChange}
+            fullWidth
             />
             <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                id="email"
-                label="Digite um nome de usuario"
-                name="email"
-                type="text"
-                value={form.email}
-                onChange={handleInputChange}
-                fullWidth
+            variant="outlined"
+            margin="normal"
+            required
+            id="email"
+            label="Informe seu e-mail"
+            name="email"
+            type="email"
+            value={form.email}
+            onChange={handleInputChange}
+            fullWidth
             />
             <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                id="nickname"
-                label="Informe seu e-mail"
-                name="nickname"
-                type="text"
-                value={form.nickname}
-                onChange={handleInputChange}
-                fullWidth
+            variant="outlined"
+            margin="normal"
+            required
+            id="nickname"
+            label="Informe um nickname"
+            name="nickname"
+            type="text"
+            value={form.nickname}
+            onChange={handleInputChange}
+            fullWidth
             />
             <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                name="password"
-                label="Informe uma senha"
-                type="password"
-                id="password"
-                value={form.password}
-                onChange={handleInputChange}
-                fullWidth
+            variant="outlined"
+            margin="normal"
+            required
+            name="password"
+            label="Informe uma senha"
+            type="password"
+            id="password"
+            value={form.password}
+            onChange={handleInputChange}
+            fullWidth
             />
             <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
             >
-                Cadastrar
+            Cadastrar
             </Button>
             </form>
         </div>
-    );
+    )
 }
 
-export default SignUpPage;
+export default SignUpPage
